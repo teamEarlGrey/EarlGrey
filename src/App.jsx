c742df407d0044d5f17501d4455d007fbd9aa73f
tomatoaas <tomomi.314159265@gmail.com>
2022-05-13 21:58:39 +0900

    [test]SPAをやってみました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-export const App = () => {
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Users from "./Users";
+import "./index.css";
+
+const App = () => {
   return (
-    <div className="text-red-400">
-    こんばんは
-    </div>
-  )
-  
+    <BrowserRouter>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="about" className={'text-red-600'}>
+          About
+        </Link>
+        <Link to="users">Users</Link>
+
+        <Routes>
+          <Route path="/" />
+
+          <Route path="about" element={<About />} />
+
+          <Route path="users" element={<Users />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
