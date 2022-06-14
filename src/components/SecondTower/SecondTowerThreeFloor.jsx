0575b98ff82345b40519edfe59c365ba027261f4
tomatoaas <tomomi.314159265@gmail.com>
2022-06-14 11:25:54 +0900

    [fix]２号館３階のトイレなどの位置を微調整

--- src/components/SecondTower/SecondTowerThreeFloor.jsx
+++ src/components/SecondTower/SecondTowerThreeFloor.jsx
@@ -20,7 +20,7 @@ const SecondTowerThreeFloor = () => {
               2303ノートPC対応教室
             </div>
             <div
-              className={`w-[120px] h-[120px] border-2 border-[#000] my-0.5 ml-0.5 px-1 py-[32px]`}
+              className={`w-[120px] h-[116px] border-2 border-[#000] my-0.5 ml-0.5 px-1 py-[32px]`}
             >
               女子学生専用パウダールーム
             </div>
@@ -28,7 +28,7 @@ const SecondTowerThreeFloor = () => {
           <div className={`flex-col`}>
             <div className={`w-[40px] h-[240px] m-0.5 px-2 py-1`}></div>
             <div
-              className={`w-[155px] h-[120px] border-y-2 border-r-2 border-[#000] my-0.5 absolute py-[45px]`}
+              className={`w-[155px] h-[116px] border-y-2 border-r-2 border-[#000] my-0.5 absolute py-[45px]`}
             >
               図書室
             </div>
@@ -44,7 +44,7 @@ const SecondTowerThreeFloor = () => {
             className={`w-[80px] h-[120px] border-l-2 border-b-2 border-[#000] px-2`}
           ></div>
           <div
-            className={`w-[80px] h-[252px] border-r-2 border-b-2 border-[#000] flex flex-row`}
+            className={`w-[80px] h-[248px] border-r-2 border-b-2 border-[#000] flex justify-between`}
           >
             <div className={`w-[40px] h-[252px]`}>
               <img
@@ -53,24 +53,24 @@ const SecondTowerThreeFloor = () => {
                 alt="階段"
               />
             </div>
-            <div className={`w-[40px] flex-col`}>
+            <div className={`w-[33px] flex-col mr-0.5`}>
               <div
-                className={`w-[35px] h-[48px] border-2 border-[#000] mt-0.5 mr-0.5`}
+                className={`w-[33px] h-[51px] border-2 border-[#000] mt-0.5 mr-0.5`}
               >
                 WC (女)
               </div>
               <div
-                className={`w-[35px] h-[48px] border-x-2 border-[#000] mr-0.5`}
+                className={`w-[33px] h-[47px] border-x-2 border-[#000] mr-0.5`}
               >
                 WC (男)
               </div>
               <div
-                className={`w-[35px] h-[30px] border-2 border-[#000] mr-0.5`}
+                className={`w-[33px] h-[29.5px] border-2 border-[#000] mr-0.5`}
               >
                 EV
               </div>
               <div
-                className={`w-[35px] h-[120px] border-x-2 border-b-2 border-[#000] mr-0.5 mb-0.5`}
+                className={`w-[33px] h-[115px] border-x-2 border-b-2 border-[#000] mr-0.5 mb-0.5`}
               ></div>
             </div>
           </div>
