aa6418dfedc81efc1a6113599c430ce4dbb85c75
tomatoaas <tomomi.314159265@gmail.com>
2022-07-21 23:04:35 +0900

    [add]ハンバーガーメニューを追加した。

--- src/pages/TopPage.jsx
+++ src/pages/TopPage.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useMedia from "use-media";
 import { Link } from "react-router-dom";
 
 const TopPage = () => {
   const max1300 = useMedia({ maxWidth: "1300px" });
   const max750 = useMedia({ maxWidth: "750px" });
+  // ハンバーガーメニュー表示するかしないかの変数
+  const [openMenu, setOpenMenu] = React.useState(false);
+
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  // ハンバーガーメニューのプラス押したか押してないか判定
+  const [openTower, setOpenTower] = React.useState(false);
+
   return (
     <>
       <header className={`sticky z-50 top-0`}>
         <div
-          className={`${
-            max750 ? `p-3` : `p-3`
-          } border-b-2 text-xl font-medium flex bg-white `}
+          className={`p-2 border-b-2 text-xl font-medium flex bg-white ${
+            max750 && `justify-between`
+          } `}
         >
           <a
             href="http://localhost:3000/"
@@ -20,7 +30,21 @@ const TopPage = () => {
             <img src="./images/logo.png" alt="KitAru" className={`px-3`} />
           </a>
           {max750 ? (
-            ``
+            <div className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-6 w-6 text-[#7FD4FF] flex justify-end`}
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                onClick={menuFunction}
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
           ) : (
             <>
               <a href="#aki" className="px-4 py-2">
@@ -32,7 +56,99 @@ const TopPage = () => {
             </>
           )}
         </div>
+        {/* ハンバーガーメニュー */}
+        {openMenu && (
+          <div
+            className={`flex flex-row absolute w-full bg-[#C6ECFF] text-white py-3`}
+          >
+            <ul className={`w-full`}>
+              <li>
+                <div
+                  className={`px-5 flex justify-between border-b-2 border-[#80D4FF] text-shadow`}
+                >
+                  <a
+                    href="#aki"
+                    onClick={() => {
+                      menuFunction();
+                      setOpenTower(false);
+                    }}
+                  >
+                    空き教室
+                  </a>
+                  {/*
+                      plusボタンが押された時(openmenuがtrue)minus.pngに変更
+                      逆も同じ動作
+                  */}
+                  {openTower ? (
+                    <img
+                      src="../images/minus.png"
+                      alt="minus"
+                      className={`object-contain w-4`}
+                      onClick={() => setOpenTower(!openTower)}
+                    />
+                  ) : (
+                    <img
+                      src="../images/plus.png"
+                      alt="plus"
+                      className={`object-contain w-4`}
+                      onClick={() => setOpenTower(!openTower)}
+                    />
+                  )}
+                </div>
+                {/* plusボタンが押された時、号館を表示、
+                    minusボタンが押された時は非表示
+                */}
+                {openTower && (
+                  <>
+                    <ul>
+                      <li className={`mx-10 border-b-2 border-[#80D4FF]`}>
+                        <a
+                          href="/first"
+                          className="text-shadow"
+                          onClick={menuFunction}
+                        >
+                          １号館
+                        </a>
+                      </li>
+                      <li className={`mx-10 border-b-2 border-[#80D4FF]`}>
+                        <a
+                          href="/second"
+                          className="text-shadow"
+                          onClick={menuFunction}
+                        >
+                          ２号館
+                        </a>
+                      </li>
+                      <li className={`mx-10 border-b-2 border-[#80D4FF]`}>
+                        <a
+                          href="/third"
+                          className="text-shadow"
+                          onClick={menuFunction}
+                        >
+                          ３号館
+                        </a>
+                      </li>
+                    </ul>
+                  </>
+                )}
+              </li>
+              <li>
+                <a
+                  href="#setubi"
+                  className="px-5 block text-shadow border-b-2 border-[#80D4FF]"
+                  onClick={() => {
+                    menuFunction();
+                    setOpenTower(false);
+                  }}
+                >
+                  設備
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </header>
+
       <div
         id="top"
         className={`${
@@ -122,8 +238,7 @@ const TopPage = () => {
               } bg-[#F4F5F5] text-xl font-medium flex justify-between flex-wrap`}
             >
               {/* ブラウザサイズが750pxより小さい時 */}
-              {max750 ? <div></div>
-              : <></>}
+              {max750 ? <div></div> : <></>}
               {/* ブラウザサイズが750pxより大きい時 */}
               <img
                 src="./images/setubi.png"
